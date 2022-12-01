2ba69deb3d35c4778984db14a63ebbd23b0e92d9
fbeclin <60063337+fbeclin@users.noreply.github.com>
2022-12-01 21:32:41 +0100

    prepare for day 2

---
+++ day_02/run.py
@@ -0,0 +1,27 @@
+import timeit
+
+INPUT_FILEPATH = "./na.txt"
+
+
+def print_header():
+    print("================")
+    print("= AoC - Day 02 =")
+    print("================")
+
+
+def round_1():
+    pass
+
+
+def round_2():
+    pass
+
+
+def main():
+    print_header()
+
+
+if __name__ == "__main__":
+    start_time = timeit.default_timer()
+    main()
+    print(f"Execution time : {timeit.default_timer() - start_time}")
