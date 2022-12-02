1219affe92a3bacefe6acbd0c5458c2379b5c8d3
fbeclin <60063337+fbeclin@users.noreply.github.com>
2022-12-02 14:55:58 +0100

    round 1

--- day_02/run.py
+++ day_02/run.py
@@ -1,6 +1,19 @@
+from enum import Enum
 import timeit
 
-INPUT_FILEPATH = "./na.txt"
+INPUT_FILEPATH = "./input.txt"
+
+
+class RoundScore(Enum):
+    LOSE = 0
+    DRAW = 3
+    WIN = 6
+
+
+class ActionScore(Enum):
+    X = 1
+    Y = 2
+    Z = 3
 
 
 def print_header():
@@ -9,8 +22,41 @@ def print_header():
     print("================")
 
 
-def round_1():
-    pass
+"""
+Opponent: A = Rock, B = Paper, C = Scissors
+You: X = Rock, Y = Paper, Z = Scissors
+A / X = Draw
+A / Y = Win
+A / Z = Lose
+B / X = Lose
+B / Y = Draw
+B / Z = Win
+C / X = Win
+C / Y = Lose
+C / Z = Draw
+"""
+
+round_map = {
+    "A X": RoundScore.DRAW,
+    "A Y": RoundScore.WIN,
+    "A Z": RoundScore.LOSE,
+    "B X": RoundScore.LOSE,
+    "B Y": RoundScore.DRAW,
+    "B Z": RoundScore.WIN,
+    "C X": RoundScore.WIN,
+    "C Y": RoundScore.LOSE,
+    "C Z": RoundScore.DRAW,
+}
+
+
+def play_round(line: str) -> int:
+    return round_map[line].value + ActionScore[line[2]].value
+
+
+def round_1(filename: str):
+    with open(filename) as f:
+        total_score = sum([play_round(line.strip()) for line in f.readlines()])
+        print(f"Total_score: {total_score}")
 
 
 def round_2():
@@ -19,6 +65,7 @@ def round_2():
 
 def main():
     print_header()
+    round_1(INPUT_FILEPATH)
 
 
 if __name__ == "__main__":
