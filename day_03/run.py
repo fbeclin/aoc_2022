cf07c0fadf8da23b25b9e68fa71ac11887b47dd7
fbeclin <60063337+fbeclin@users.noreply.github.com>
2022-12-03 10:27:05 +0100

    prepare day 03

---
+++ day_03/run.py
@@ -0,0 +1,28 @@
+import timeit
+from itertools import islice
+
+INPUT_FILEPATH = "./input1.txt"
+
+
+def print_header():
+    print("================")
+    print("= AoC - Day 03 =")
+    print("================")
+
+
+def round_1():
+    pass
+
+
+def round_2():
+    pass
+
+
+def main():
+    print_header()
+
+
+if __name__ == "__main__":
+    start_time = timeit.default_timer()
+    main()
+    print(f"Execution time : {timeit.default_timer() - start_time}")
