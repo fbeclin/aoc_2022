00e901f17595c3d50f0fb2c9238e21339f25ff35
fbeclin <60063337+fbeclin@users.noreply.github.com>
2022-12-04 11:07:35 +0100

    without set

--- day_04/run.py
+++ day_04/run.py
@@ -42,16 +42,73 @@ def round_1(filename: str):
         print(f"sum_fully_countained: {sum_fully_countained}")
 
 
+def round_1_no_set(filename: str):
+    with open(filename) as f:
+        sum_fully_countained = sum(
+            [
+                is_fully_contained_no_set(line.strip().split(","))
+                for line in f.readlines()
+            ]
+        )
+        print(f"sum_fully_countained: {sum_fully_countained}")
+
+
 def round_2(filename: str):
     with open(filename) as f:
         sum_overlap = sum([overlap(line.strip().split(",")) for line in f.readlines()])
         print(f"sum_overlap: {sum_overlap}")
 
 
+def round_2_no_set(filename: str):
+    with open(filename) as f:
+        sum_overlap = sum(
+            [overlap_no_set(line.strip().split(",")) for line in f.readlines()]
+        )
+        print(f"sum_overlap: {sum_overlap}")
+
+
+def get(range_from_file: str):
+    range_split = range_from_file.split("-")
+    return [int(range_split[0]), int(range_split[1])]
+
+
+def is_range_contained_in_other(first_range, second_range):
+    return (
+        first_range[0] >= second_range[0]
+        and first_range[0] <= second_range[1]
+        and first_range[1] >= second_range[0]
+        and first_range[1] <= second_range[1]
+    )
+
+
+def is_fully_contained_no_set(ranges: list):
+    range_1 = get(ranges[0])
+    range_2 = get(ranges[1])
+
+    # range 1 contained
+    if is_range_contained_in_other(range_1, range_2):
+        return True
+
+    # range 2 contained
+    if is_range_contained_in_other(range_2, range_1):
+        return True
+
+    return False
+
+
+def overlap_no_set(ranges: list):
+    range_1 = get(ranges[0])
+    range_2 = get(ranges[1])
+
+    return not (range_1[1] < range_2[0] or range_1[0] > range_2[1])
+
+
 def main():
     print_header()
     # round_1(INPUT_FILEPATH)
-    round_2(INPUT_FILEPATH)
+    # round_1_no_set(INPUT_FILEPATH)
+    # round_2(INPUT_FILEPATH)
+    round_2_no_set(INPUT_FILEPATH)
 
 
 if __name__ == "__main__":
