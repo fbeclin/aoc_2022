fa22dad19e4f827fb657668cea940ba4978e8a20
fbeclin <60063337+fbeclin@users.noreply.github.com>
2022-12-05 22:18:30 +0100

    first round

--- day_05/run.py
+++ day_05/run.py
@@ -1,6 +1,14 @@
 import timeit
+import re
 
-INPUT_FILEPATH = "./input1.txt"
+
+INPUT_FILEPATH = "./example.txt"
+STACK_REGEX = "( {3}|\[([A-Z])\])"
+CRANE_REGEX = "move (\d) from (\d) to (\d)"
+
+
+switch_to_crane = False
+stacks = []
 
 
 def print_header():
@@ -9,8 +17,55 @@ def print_header():
     print("================")
 
 
+def read_stacks(line: str):
+    global switch_to_crane
+    if len(line) < 3:
+        switch_to_crane = True
+    else:
+        matches = re.findall(STACK_REGEX, line)
+        if len(matches) == 3:
+            load_stacks(matches)
+
+
+def load_stacks(matches: list):
+    global stacks
+    if not stacks:
+        stacks = [[] for _ in range(len(matches))]
+
+    for i, stack in enumerate(matches):
+        if stack[1]:
+            stacks[i].insert(0, stack[1])
+
+    print(stacks)
+
+
+def read_movements(line: str):
+    global stacks
+    matches = re.findall(CRANE_REGEX, line)[0]
+    print(matches)
+    apply_movement(int(matches[0]), int(matches[1]), int(matches[2]))
+
+
+def apply_movement(nb_stack: int, from_stack: int, to_stack: int):
+    for _ in range(nb_stack):
+        stack = stacks[from_stack - 1].pop()
+        stacks[to_stack - 1].append(stack)
+
+    print(stacks)
+
+
+def get_messages():
+    global stacks
+    print("".join(s[0] for _, s in enumerate(stacks)))
+
+
 def round_1(filename: str):
-    pass
+    with open(filename) as f:
+        [
+            read_movements(line) if switch_to_crane else read_stacks(line)
+            for line in f.readlines()
+        ]
+        get_messages()
     # with open(filename) as f:
     #     sum_fully_countained = sum(
     #         [is_fully_contained(line.strip().split(",")) for line in f.readlines()]
@@ -24,7 +79,7 @@ def round_2(filename: str):
 
 def main():
     print_header()
-    # round_1(INPUT_FILEPATH)
+    round_1(INPUT_FILEPATH)
     # round_2(INPUT_FILEPATH)
 
 
