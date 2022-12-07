c01c90179b8f7b1869db82ec4e3366e683a71a70
fbeclin <60063337+fbeclin@users.noreply.github.com>
2022-12-07 23:50:06 +0100

    round 2 completed

--- day_07/run.py
+++ day_07/run.py
@@ -7,6 +7,8 @@ INPUT_FILEPATH = "./input1.txt"
 COMMAND_REGEX = "^\$ ([a-z]+) ?(.+)?"
 LS_FILE_REGEX = "^([0-9]+) (.+)"
 SIZE_THRESHOLD = 100000
+MIN_REQUIRED_SIZE = 30000000
+TOTAL_SYSTEM_SIZE = 70000000
 
 
 class Folder(object):
@@ -20,7 +22,8 @@ class Folder(object):
 
 
 current_folder: Folder = None
-sum_size = 0
+sum_size_threshold = 0
+folder_sizes = []
 
 
 def print_header():
@@ -30,18 +33,14 @@ def print_header():
 
 
 def read_command(command: str):
-    print(f"command: {command}")
-    print(f"current_folder: {current_folder}")
     match = re.match(COMMAND_REGEX, command)
     if match:
         if match[1] == "cd":
             if match[2] != "..":
-                print(f"match[2]={match[2]}")
                 go_into_folder(match[2])
             else:
                 go_into_parent_folder()
     else:
-        print("not match")
         match = re.match(LS_FILE_REGEX, command)
         if match:
             current_folder.size += int(match[1])
@@ -49,48 +48,50 @@ def read_command(command: str):
 
 def go_into_folder(foldername: str):
     global current_folder
-    print(f"go_into_folder: {foldername}")
     folder = Folder(foldername, current_folder)
     current_folder = folder
 
 
 def go_into_parent_folder():
     global current_folder
-    global sum_size
-    if current_folder.parent_folder:
-        print(current_folder)
+    global sum_size_threshold
 
+    if current_folder.parent_folder != None:
+        exiting_folder_size = current_folder.size
         if current_folder.size < SIZE_THRESHOLD:
-            sum_size += current_folder.size
+            sum_size_threshold += exiting_folder_size
 
-        current_folder.parent_folder.size += current_folder.size
+        current_folder.parent_folder.size += exiting_folder_size
         current_folder = current_folder.parent_folder
+
+        folder_sizes.append(exiting_folder_size)
 
 
 def go_back_to_root():
-    while not current_folder.parent_folder:
-        print(current_folder)
+    global sum_size_threshold
+    while current_folder.parent_folder != None:
         go_into_parent_folder()
-        input()
 
 
 def round_1(filename: str):
     with open(filename) as f:
         [read_command(line.strip()) for line in f.readlines()]
     go_back_to_root()
-    print(sum_size)
+    print(f"sum_size_threshold: {sum_size_threshold}")
 
 
 def round_2(filename: str):
-    pass
-    # with open(filename) as f:
-    #     [read_datastream(line.strip(), 14) for line in f.readlines()]
+    round_1(filename)
+    for _, v in enumerate(sorted(folder_sizes)):
+        if current_folder.size - v <= (TOTAL_SYSTEM_SIZE - MIN_REQUIRED_SIZE):
+            print(f"folder_size_to_delete: {v}")
+            break
 
 
 def main():
     print_header()
-    round_1(INPUT_FILEPATH)
-    # round_2(INPUT_FILEPATH)
+    # round_1(INPUT_FILEPATH)
+    round_2(INPUT_FILEPATH)
 
 
 if __name__ == "__main__":
