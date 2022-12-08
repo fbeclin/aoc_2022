18e90a23a7978ecd45ed5c075cf4d6c3c5a836e8
fbeclin <60063337+fbeclin@users.noreply.github.com>
2022-12-08 22:35:53 +0100

    round 2 completed

--- day_08/run.py
+++ day_08/run.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
+from functools import reduce
 import timeit
+from typing import Iterable
 
 INPUT_FILEPATH = "./input1.txt"
 
@@ -25,6 +27,32 @@ def find_tallest(grid: list[list], col: int, row: int):
     )
 
 
+def sum_until(iter: Iterable, ref_value: int):
+    nb_tree = 0
+    for _, v in iter:
+        if v < ref_value:
+            nb_tree += 1
+        else:
+            if v == ref_value:
+                nb_tree += 1
+            break
+    return nb_tree
+
+
+def calc_viewing_score(grid: list[list], col: int, row: int):
+    current_tree_height = grid[row][col]
+    left = sum_until(enumerate(reversed(grid[row][:col])), current_tree_height)
+    right = sum_until(enumerate(grid[row][col + 1 :]), current_tree_height)
+    top = sum_until(
+        enumerate(reversed([grid[i][col] for i in range(row)])), current_tree_height
+    )
+    bottom = sum_until(
+        enumerate([grid[i][col] for i in range(row + 1, len(grid))]),
+        current_tree_height,
+    )
+    return left * right * top * bottom
+
+
 def round_1(filename: str):
     with open(filename) as f:
         grid = [list(map(int, list(line.strip()))) for line in f.readlines()]
@@ -39,13 +67,21 @@ def round_1(filename: str):
 
 
 def round_2(filename: str):
-    pass
+    highest_scenic_score = 0
+    with open(filename) as f:
+        grid = [list(map(int, list(line.strip()))) for line in f.readlines()]
+        for col in range(len(grid)):
+            for row in range(len(grid)):
+                current_scenic_score = calc_viewing_score(grid, col=col, row=row)
+                if current_scenic_score > highest_scenic_score:
+                    highest_scenic_score = current_scenic_score
+    print(f"highest_scenic_score: {highest_scenic_score}")
 
 
 def main():
     print_header()
-    round_1(INPUT_FILEPATH)
-    # round_2(INPUT_FILEPATH)
+    # round_1(INPUT_FILEPATH)
+    round_2(INPUT_FILEPATH)
 
 
 if __name__ == "__main__":
