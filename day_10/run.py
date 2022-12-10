f266da78db6e705c97ef4896872246e7a52ff61c
fbeclin <60063337+fbeclin@users.noreply.github.com>
2022-12-10 13:39:01 +0100

    round 2 completed

--- day_10/run.py
+++ day_10/run.py
@@ -14,17 +14,19 @@ crt_screen = [["."] * CRT_WIDTH for _ in range(CRT_HEIGHT)]
 
 def draw():
     global x_register
-
-    print(".", end="", flush=True)
-
-    if (len(x_register) - 1) % CYCLE_STEP == 0:
+    x = x_register[len(x_register) - 2]
+    pos = (len(x_register) - 2) % CYCLE_STEP
+    if pos == 0:
         print("\r")
+        
+    print("#" if pos >= x - 1 and pos <= x + 1 else ".", end="", flush=True)
+
+
 
 
 def noop():
     x_register.append(x_register[len(x_register) - 1])
     draw()
-
 
 def add_x(command: str):
     value = int(command.split(" ")[1])
@@ -51,6 +53,7 @@ def print_header():
 def round_1(filename: str):
     with open(filename) as f:
         [run(line.strip()) for line in f.readlines()]
+        print("\r")
         print(
             sum([x_register[c - 1] * c for c in range(20, MAX_CYCLE + 1, CYCLE_STEP)])
         )
