638304eea3cc613782e298737d2e7c547934f7aa
fbeclin <60063337+fbeclin@users.noreply.github.com>
2022-12-11 20:52:19 +0100

    add operator

--- day_11/run.py
+++ day_11/run.py
@@ -1,6 +1,37 @@
 import timeit
+import re
 
 INPUT_FILEPATH = "./input1.txt"
+
+
+class Monkey(object):
+    def __init__(
+        self,
+        items,
+        operation: str,
+    ) -> None:
+        self.items = items
+
+
+def get_operation(operation: str):
+    match = re.match("Operation: new = (\d+|old) (\+|\*\-\/) (\d+|old)", operation)
+    if match:
+
+        def handle(old: int) -> int:
+            var_1 = int(match[1]) if match[1] == "old" else old
+            var_2 = int(match[3]) if match[3] == "old" else old
+
+            match match[2]:
+                case "+":
+                    return var_1 + var_2
+                case "-":
+                    return var_1 - var_2
+                case "*":
+                    return var_1 * var_2
+                case "/":
+                    return var_1 / var_2
+
+        return handle
 
 
 def print_header():
@@ -16,7 +47,9 @@ def round_1(filename: str):
 
 def main():
     print_header()
-    round_1(INPUT_FILEPATH)
+    f_op = get_operation("Operation: new = old + 8")
+    print(f_op(10))
+    # round_1(INPUT_FILEPATH)
 
 
 if __name__ == "__main__":
