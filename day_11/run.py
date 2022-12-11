6b52ea78bee7db1c3a3575425d9a0826206cbe1e
fbeclin <60063337+fbeclin@users.noreply.github.com>
2022-12-11 19:59:43 +0100

    prepare round 1

---
+++ day_11/run.py
@@ -0,0 +1,25 @@
+import timeit
+
+INPUT_FILEPATH = "./input1.txt"
+
+
+def print_header():
+    print("================")
+    print("= AoC - Day 11 =")
+    print("================")
+
+
+def round_1(filename: str):
+    with open(filename) as f:
+        pass
+
+
+def main():
+    print_header()
+    round_1(INPUT_FILEPATH)
+
+
+if __name__ == "__main__":
+    start_time = timeit.default_timer()
+    main()
+    print(f"Execution time : {timeit.default_timer() - start_time}")
