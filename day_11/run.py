34e9c067a4ea0141b146d649b956fd283844eff5
fbeclin <60063337+fbeclin@users.noreply.github.com>
2022-12-11 23:37:24 +0100

    round 2 but slow

--- day_11/run.py
+++ day_11/run.py
@@ -4,8 +4,6 @@ import timeit
 import re
 
 INPUT_FILEPATH = "./example.txt"
-PRINT = True
-
 
 class Monkey(object):
     def __init__(
@@ -26,7 +24,7 @@ class Monkey(object):
 
 
 def get_operation(
-    operation: str, divisible_by: int, to_monkey_if_true: int, to_monkey_if_false: int
+    operation: str, divisible_by: int, to_monkey_if_true: int, to_monkey_if_false: int, divide_by_3: bool = False
 ):
     match = re.match("(\d+|old) (\+|\*|\-|\/) (\d+|old)", operation)
     if not match:
@@ -47,7 +45,8 @@ def get_operation(
             case "/":
                 new_worry = var_1 / var_2
 
-        new_worry = int(new_worry / 3)
+        if divide_by_3:
+            new_worry = int(new_worry / 3)
 
         return (
             (to_monkey_if_true, new_worry)
@@ -62,7 +61,7 @@ def read(f: TextIOWrapper, prefix: str):
     return f.readline().removeprefix(prefix).strip()
 
 
-def get_monkey(line: str, f: TextIOWrapper):
+def get_monkey(line: str, f: TextIOWrapper, divide_by_3: bool = False):
     if line.startswith("Monkey"):
         starting_items = list(map(int, read(f, "  Starting items: ").split(",")))
         operation = get_operation(
@@ -70,6 +69,7 @@ def get_monkey(line: str, f: TextIOWrapper):
             int(read(f, "  Test: divisible by ")),
             int(read(f, "    If true: throw to monkey ")),
             int(read(f, "    If false: throw to monkey ")),
+            divide_by_3
         )
 
         return Monkey(items=starting_items, operation_fct=operation)
@@ -92,20 +92,39 @@ def round_1(filename: str):
                 break
             line = line.strip()
             if line != "":
-                monkeys.append(get_monkey(line, f))
+                monkeys.append(get_monkey(line, f, True))
 
-        for r in range(20):
-            for i, m in enumerate(monkeys):
+        for _ in range(20):
+            for _, m in enumerate(monkeys):
                 m.inspect_and_throw(monkeys=monkeys)
 
         monkeys = sorted(monkeys, key=lambda x: x.number_of_inspection, reverse=True)
         print(monkeys[0].number_of_inspection * monkeys[1].number_of_inspection)
 
 
+def round_2(filename: str):
+    with open(filename) as f:
+        monkeys: list[Monkey] = []
+        while True:
+            line = f.readline()
+            if not line:
+                break
+            line = line.strip()
+            if line != "":
+                monkeys.append(get_monkey(line, f))
+
+        for i in range(10000):
+            print(f"round: {i}")
+            for _, m in enumerate(monkeys):
+                m.inspect_and_throw(monkeys=monkeys)
+
+        monkeys = sorted(monkeys, key=lambda x: x.number_of_inspection, reverse=True)
+        print(monkeys[0].number_of_inspection * monkeys[1].number_of_inspection)
+
 def main():
     print_header()
-    round_1(INPUT_FILEPATH)
-
+    # round_1(INPUT_FILEPATH)
+    round_2(INPUT_FILEPATH)
 
 if __name__ == "__main__":
     start_time = timeit.default_timer()
