ff5b6496919a41f5b7e9ec189f3c4592a7bcb616
fbeclin <60063337+fbeclin@users.noreply.github.com>
2022-12-12 18:25:32 +0100

    load heightmap

--- day_12/run.py
+++ day_12/run.py
@@ -2,15 +2,36 @@ import timeit
 
 INPUT_FILEPATH = "./example.txt"
 
+
+heightmap = []
+start_pos = (0, 0)
+end_pos = (0, 0)
+
+
 def print_header():
     print("================")
     print("= AoC - Day 12 =")
     print("================")
 
 
+def load_heightmap(line: str):
+    global start_pos, end_pos
+    row = []
+    for i, c in enumerate(line):
+        if c == "S":
+            start_pos = (len(heightmap), i)
+            c = "a"
+        elif c == "E":
+            end_pos = (len(heightmap), i)
+            c = "z"
+        row.append(c)
+    heightmap.append(row)
+
+
 def round_1(filename: str):
     with open(filename) as f:
-        pass
+        [load_heightmap(line.strip()) for line in f.readlines()]
+        print("start:" + str(start_pos) + " / end:" + str(end_pos))
 
 
 def round_2(filename: str):
