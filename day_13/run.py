67055b85ab4e006b362cba71407bfcb6a81bbf65
fbeclin <60063337+fbeclin@users.noreply.github.com>
2022-12-14 14:06:09 +0100

    round 1 - without print

--- day_13/run.py
+++ day_13/run.py
@@ -8,41 +8,53 @@ INPUT_FILEPATH = "./example.txt"
 
 
 def compare(left: list, right: list, depth: int = 0):
-    left_padding = " "*depth
+    left_padding = " " * depth
     print(f"{left_padding}- Compare {left} vs {right}")
     i = 0
     while i < len(left) and i < len(right):
         if isinstance(left[i], int) and isinstance(right[i], int):
-            left_padding = " "*(depth+1)
+            left_padding = " " * (depth + 1)
             print(f"{left_padding}- Compare {left[i]} vs {right[i]}")
 
             if left[i] > right[i]:
-                left_padding = " "*(depth+2)
-                print(f"{left_padding}- Right side is smaller, so inputs are NOT in the right order")
+                left_padding = " " * (depth + 2)
+                print(
+                    f"{left_padding}- Right side is smaller, so inputs are NOT in the right order"
+                )
                 return False
             elif left[i] < right[i]:
-                left_padding = " "*(depth+2)
-                print(f"{left_padding}- Left side is smaller, so inputs are in the right order")
+                left_padding = " " * (depth + 2)
+                print(
+                    f"{left_padding}- Left side is smaller, so inputs are in the right order"
+                )
                 return True
         elif isinstance(left[i], list) and isinstance(right[i], list):
-            left_padding = " "*(depth+1)
-            return compare(left[i], right[i], depth+1)
+            left_padding = " " * (depth + 1)
+            return compare(left[i], right[i], depth + 1)
         else:
-            left_padding = " "*(depth+1)
+            left_padding = " " * (depth + 1)
             if isinstance(left[i], int):
-                print(f"{left_padding}- Mixed types; convert left to [{left[i]}] and retry comparison")
-                return compare([left[i]], right[i], depth+1)
+                print(
+                    f"{left_padding}- Mixed types; convert left to [{left[i]}] and retry comparison"
+                )
+                return compare([left[i]], right[i], depth + 1)
             else:
-                print(f"{left_padding}- Mixed types; convert right to [{right[i]}] and retry comparison")
-                return compare(left[i], [right[i]], depth+1)
-        i+=1
+                print(
+                    f"{left_padding}- Mixed types; convert right to [{right[i]}] and retry comparison"
+                )
+                return compare(left[i], [right[i]], depth + 1)
+        i += 1
 
     if i != len(left) and i >= len(right):
-        left_padding = " "*(depth+1)
-        print(f"{left_padding}- Right side ran out of items, so inputs are NOT in the right order")
+        left_padding = " " * (depth + 1)
+        print(
+            f"{left_padding}- Right side ran out of items, so inputs are NOT in the right order"
+        )
         return False
-    left_padding = " "*(depth+1)
-    print(f"{left_padding}- Left side ran out of items, so inputs are in the right order")
+    left_padding = " " * (depth + 1)
+    print(
+        f"{left_padding}- Left side ran out of items, so inputs are in the right order"
+    )
     return True
 
 
@@ -54,17 +66,36 @@ def print_header():
 
 def round_1(filename: str):
     with open(filename) as f:
-
         i = 1
         for pair in [
-            json.loads("{ \"left\":" + lines[0].strip() +  ", \"right\":" + lines[1].strip() + " }")
+            json.loads(
+                '{ "left":' + lines[0].strip() + ', "right":' + lines[1].strip() + " }"
+            )
             for lines in grouper(f, 3, fillvalue="")
         ]:
             print(f"== Pair {i} ==")
-            compare(pair["left"],pair["right"])
+            compare(pair["left"], pair["right"])
             print("")
-            i+=1
-        pass
+            i += 1
+
+
+def round_1_without_print(filename: str):
+    with open(filename) as f:
+        print(
+            sum(
+                compare(pair["left"], pair["right"])
+                for pair in [
+                    json.loads(
+                        '{ "left":'
+                        + lines[0].strip()
+                        + ', "right":'
+                        + lines[1].strip()
+                        + " }"
+                    )
+                    for lines in grouper(f, 3, fillvalue="")
+                ]
+            )
+        )
 
 
 def round_2(filename: str):
