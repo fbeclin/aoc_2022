ef683bdd1df434e3ce7f7dbf8a91c66b9925bb7f
fbeclin <60063337+fbeclin@users.noreply.github.com>
2022-12-14 14:12:01 +0100

    no print

--- day_13/run.py
+++ day_13/run.py
@@ -8,53 +8,24 @@ INPUT_FILEPATH = "./example.txt"
 
 
 def compare(left: list, right: list, depth: int = 0):
-    left_padding = " " * depth
-    print(f"{left_padding}- Compare {left} vs {right}")
     i = 0
     while i < len(left) and i < len(right):
         if isinstance(left[i], int) and isinstance(right[i], int):
-            left_padding = " " * (depth + 1)
-            print(f"{left_padding}- Compare {left[i]} vs {right[i]}")
-
             if left[i] > right[i]:
-                left_padding = " " * (depth + 2)
-                print(
-                    f"{left_padding}- Right side is smaller, so inputs are NOT in the right order"
-                )
                 return False
             elif left[i] < right[i]:
-                left_padding = " " * (depth + 2)
-                print(
-                    f"{left_padding}- Left side is smaller, so inputs are in the right order"
-                )
                 return True
         elif isinstance(left[i], list) and isinstance(right[i], list):
-            left_padding = " " * (depth + 1)
             return compare(left[i], right[i], depth + 1)
         else:
-            left_padding = " " * (depth + 1)
             if isinstance(left[i], int):
-                print(
-                    f"{left_padding}- Mixed types; convert left to [{left[i]}] and retry comparison"
-                )
                 return compare([left[i]], right[i], depth + 1)
             else:
-                print(
-                    f"{left_padding}- Mixed types; convert right to [{right[i]}] and retry comparison"
-                )
                 return compare(left[i], [right[i]], depth + 1)
         i += 1
 
     if i != len(left) and i >= len(right):
-        left_padding = " " * (depth + 1)
-        print(
-            f"{left_padding}- Right side ran out of items, so inputs are NOT in the right order"
-        )
         return False
-    left_padding = " " * (depth + 1)
-    print(
-        f"{left_padding}- Left side ran out of items, so inputs are in the right order"
-    )
     return True
 
 
