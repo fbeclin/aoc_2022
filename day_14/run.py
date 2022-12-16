cc2371fa054ca790483689f7d6d4f3f8145da8c7
fbeclin <60063337+fbeclin@users.noreply.github.com>
2022-12-16 20:35:16 +0100

    day 14 prepare

---
+++ day_14/run.py
@@ -0,0 +1,36 @@
+from __future__ import annotations
+import functools
+from itertools import chain
+from more_itertools import grouper
+import timeit
+import json
+
+INPUT_FILEPATH = "./example.txt"
+# INPUT_FILEPATH = "./input1.txt"
+
+
+def print_header():
+    print("================")
+    print("= AoC - Day 14 =")
+    print("================")
+
+
+def round_1(filename: str):
+    with open(filename) as f:
+       pass
+
+def round_2(filename: str):
+    with open(filename) as f:
+        pass
+
+
+def main():
+    print_header()
+    round_1(INPUT_FILEPATH)
+    # round_2(INPUT_FILEPATH)
+
+
+if __name__ == "__main__":
+    start_time = timeit.default_timer()
+    main()
+    print(f"Execution time : {timeit.default_timer() - start_time}")
