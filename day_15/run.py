220aa15d3b60eff96acf1f79b48d6fca61580da8
fbeclin <60063337+fbeclin@users.noreply.github.com>
2022-12-17 22:54:01 +0100

    round 1

--- day_15/run.py
+++ day_15/run.py
@@ -13,6 +13,7 @@ INPUT_FILEPATH = "./example.txt"
 
 sensors_with_closest_beacon = {}
 beacon_with_sensors = {}
+sensors_coverage = set([])
 
 
 # Sensor at x=2, y=18: closest beacon is at x=-2, y=15
@@ -32,29 +33,97 @@ def print_header():
     print("================")
 
 
+def get_coverage(sensor: tuple(int, int), x: tuple(int, int), y: tuple(int, int)):
+    beacon = sensors_with_closest_beacon[sensor]
+    distance = abs(beacon[0] - sensor[0]) + abs(beacon[1] - sensor[1])
+
+    for dist in range(distance + 1):
+        for col in range(distance + 1 - dist):
+            c = (sensor[0] - col, sensor[1] - dist)
+            if c not in sensors_coverage:
+                sensors_coverage.add(c)
+
+        for col in range(distance + 1 - dist):
+            c = (sensor[0] + col, sensor[1] - dist)
+            if c not in sensors_coverage:
+                sensors_coverage.add(c)
+
+        for col in range(distance + 1 - dist):
+            c = (sensor[0] + col, sensor[1] + dist)
+            if c not in sensors_coverage:
+                sensors_coverage.add(c)
+
+        for col in range(distance + 1 - dist):
+            c = (sensor[0] - col, sensor[1] + dist)
+            if c not in sensors_coverage:
+                sensors_coverage.add(c)
+
+
 def draw(x, y):
     print(x, y)
-    for row in range(y[0], y[1]+1):
+    for row in range(y[0], y[1] + 1):
         print(f"{row:4} ", end="")
-        for col in range(x[0], x[1]+1):
-            symb = "B" if beacon_with_sensors.get((col, row)) else "S" if sensors_with_closest_beacon.get((col, row)) else "."
+        for col in range(x[0], x[1] + 1):
+            symb = (
+                "B"
+                if beacon_with_sensors.get((col, row))
+                else "S"
+                if sensors_with_closest_beacon.get((col, row))
+                else "#"
+                if (col, row) in sensors_coverage
+                else "."
+            )
             print(symb, end="")
         print("")
-            
 
+
+def get_number(row: int, x: tuple(int, int)):
+    return len(
+        [
+            x
+            for x in range(x[0], x[1] + 1)
+            if beacon_with_sensors.get((x, row))
+            or sensors_with_closest_beacon.get((x, row))
+            or (x, row) in sensors_coverage
+        ]
+    ) - 1
 
 
 def round_1(filename: str):
     with open(filename) as f:
         [load_sensor(line.strip()) for line in f.readlines()]
 
-        min_x = min(e[0] for e in chain(beacon_with_sensors.keys(),sensors_with_closest_beacon.keys()))
-        max_x = max(e[0] for e in chain(beacon_with_sensors.keys(),sensors_with_closest_beacon.keys()))
+        min_x = min(
+            e[0]
+            for e in chain(
+                beacon_with_sensors.keys(), sensors_with_closest_beacon.keys()
+            )
+        )
+        max_x = max(
+            e[0]
+            for e in chain(
+                beacon_with_sensors.keys(), sensors_with_closest_beacon.keys()
+            )
+        )
 
-        min_y = min(e[1] for e in chain(beacon_with_sensors.keys(),sensors_with_closest_beacon.keys()))
-        max_y = max(e[1] for e in chain(beacon_with_sensors.keys(),sensors_with_closest_beacon.keys()))
+        min_y = min(
+            e[1]
+            for e in chain(
+                beacon_with_sensors.keys(), sensors_with_closest_beacon.keys()
+            )
+        )
+        max_y = max(
+            e[1]
+            for e in chain(
+                beacon_with_sensors.keys(), sensors_with_closest_beacon.keys()
+            )
+        )
+        x = (min_x, max_x)
+        y = (min_y, max_y)
 
-        draw((min_x, max_x), (min_y, max_y))
+        [get_coverage(s, x, y) for s in sensors_with_closest_beacon.keys()]
+        print(get_number(10, x))
+        # draw(x, y)
 
 
 def round_2(filename: str):
