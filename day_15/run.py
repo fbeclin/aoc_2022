de2287f9b37466ffa2dd376def42b4734ada6a00
fbeclin <60063337+fbeclin@users.noreply.github.com>
2022-12-18 10:05:17 +0100

    round 1 completed

--- day_15/run.py
+++ day_15/run.py
@@ -1,5 +1,4 @@
 from __future__ import annotations
-from dataclasses import dataclass
 from itertools import chain
 
 # from more_itertools import grouper
@@ -8,11 +7,10 @@ import re
 
 
 PATTERN = "Sensor at x=(.+), y=(.+): closest beacon is at x=(.+), y=(.+)"
-INPUT_FILEPATH = "./example.txt"
-# INPUT_FILEPATH = "./input1.txt"
+# INPUT_FILEPATH = "./example.txt"
+INPUT_FILEPATH = "./input1.txt"
 
 sensors = {}
-sensors_coverage = set([])
 
 
 # Sensor at x=2, y=18: closest beacon is at x=-2, y=15
@@ -31,60 +29,23 @@ def print_header():
     print("================")
 
 
-def get_coverage(sensor: tuple(int, int), x: tuple(int, int), y: tuple(int, int)):
-    beacon = sensors[sensor]
-    distance = abs(beacon[0] - sensor[0]) + abs(beacon[1] - sensor[1])
+def get_row_coverage(row:int):
+    row_range = []
+    for sensor in sensors.keys():
+        # get beacon
+        beacon = sensors[sensor]
+        # manatthan dist = |Xb - Xa| + |Yb - Ya|
+        # dist between beacon and sensor
+        dist = abs(beacon[0] - sensor[0]) + abs(beacon[1] - sensor[1])
 
-    for dist in range(distance + 1):
-        for col in range(distance + 1 - dist):
-            c = (sensor[0] - col, sensor[1] - dist)
-            if c not in sensors_coverage:
-                sensors_coverage.add(c)
+        x_range = dist - abs(sensor[1] - row)
+        if x_range > 0:
+            # both sides
+            row_range.append((sensor[0] - x_range, sensor[0] + x_range ))
 
-            c = (sensor[0] + col, sensor[1] - dist)
-            if c not in sensors_coverage:
-                sensors_coverage.add(c)
-
-            c = (sensor[0] + col, sensor[1] + dist)
-            if c not in sensors_coverage:
-                sensors_coverage.add(c)
-
-            c = (sensor[0] - col, sensor[1] + dist)
-            if c not in sensors_coverage:
-                sensors_coverage.add(c)
-
-
-def draw(x, y):
-    print(x, y)
-    for row in range(y[0], y[1] + 1):
-        print(f"{row:4} ", end="")
-        for col in range(x[0], x[1] + 1):
-            symb = (
-                "B"
-                if (col, row) in sensors.values()
-                else "S"
-                if sensors.get((col, row))
-                else "#"
-                if (col, row) in sensors_coverage
-                else "."
-            )
-            print(symb, end="")
-        print("")
-
-
-def get_number(row: int, x: tuple(int, int)):
-    return (
-        len(
-            [
-                x
-                for x in range(x[0], x[1] + 1)
-                if (x, row) in sensors.values()
-                or sensors.get((x, row))
-                or (x, row) in sensors_coverage
-            ]
-        )
-        - 1
-    )
+    # find min and max
+    r = sorted(list(chain(*row_range)))
+    return r[len(r) -1] - r[0]
 
 
 def round_1(filename: str):
@@ -99,10 +60,8 @@ def round_1(filename: str):
         x = (min_x, max_x)
         y = (min_y, max_y)
         print(x, y)
+        print("row coverage: " + str(get_row_coverage(2000000)))
 
-        [get_coverage(s, x, y) for s in sensors.keys()]
-        # print(get_number(10, x))
-        draw(x, y)
 
 
 def round_2(filename: str):
