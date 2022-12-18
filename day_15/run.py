4dc6c09f2204c5e062bc3fb6a2cc720ca47c8eb7
fbeclin <60063337+fbeclin@users.noreply.github.com>
2022-12-18 21:39:13 +0100

    Day 15 (#18)
    * load data
    
    * draw grid
    
    * draw grid
    
    * round 1
    
    * remove useless stuff
    
    * round 1 completed
    
    * round 2 completed

--- day_15/run.py
+++ day_15/run.py
@@ -1,152 +1,130 @@
 from __future__ import annotations
-import functools
 from itertools import chain
-from operator import itemgetter
-from more_itertools import grouper
-import timeit
-import json
+from multiprocessing import Pool
 
+# from more_itertools import grouper
+import timeit
+import re
+
+
+PATTERN = "Sensor at x=(.+), y=(.+): closest beacon is at x=(.+), y=(.+)"
 # INPUT_FILEPATH = "./example.txt"
 INPUT_FILEPATH = "./input1.txt"
 
-MIN_X = 200
-MAX_X = 800
+sensors = {}
+beacons = {}
+distances = {}
 
-SAND_START = (500, 0)
+# Sensor at x=2, y=18: closest beacon is at x=-2, y=15
+def load_sensor(line: str):
+    search = re.search(PATTERN, line)
+    sensor = (int(search.group(1)), int(search.group(2)))
+    beacon = (int(search.group(3)), int(search.group(4)))
 
-rocks = set([])
-sands = set([])
+    # store relationship and their position
+    sensors[sensor] = beacon
+    # store manathan distance
+    distance = abs(beacon[0] - sensor[0]) + abs(beacon[1] - sensor[1])
+    distances[sensor] = distance
 
+
+def find_excluded_beacon(max_row: int):
+    for s, sensor in enumerate(sensors.keys()):
+        x,y = sensor
+        distance = distances[sensor] + 1
+        print(s, " sensor:", sensor)
+
+        for i in range(distance + 1):
+            if (x - distance + i >= 0):
+                if (y - i >= 0) :
+                    # left-top
+                    point_to_check = (x - distance + i, y - i )
+                    # print("# left-top:" + str(point_to_check))
+                    if not is_covered_by_another_sensor(point_to_check, sensor):
+                        return point_to_check
+                if (y + i <= max_row) :
+                    # left-bottom
+                    point_to_check = (x - distance + i, y + i )
+                    # print("# left-bottom:" + str(point_to_check))
+                    if not is_covered_by_another_sensor(point_to_check, sensor):
+                        return point_to_check
+            
+            if (x + distance - i <= max_row):
+                if (y - i >= 0) :
+                    # right-top
+                    point_to_check = (x + distance - i, y - i )
+                    # print("# right-top:" + str(point_to_check))
+                    if not is_covered_by_another_sensor(point_to_check, sensor):
+                        return point_to_check
+                if (y + i <= max_row) :
+                    # right-bottom
+                    point_to_check = (x + distance - i, y + i )
+                    # print("# right-bottom:" + str(point_to_check))
+                    if not is_covered_by_another_sensor(point_to_check, sensor):
+                        return point_to_check
+
+
+def is_covered_by_another_sensor(point_to_check: tuple(int,int), current_sensor: tuple(int, int)):
+    for sensor in sensors.keys():
+        if sensor != current_sensor:
+            # get distance of current sensor
+            dist = distances[sensor]
+            # get distance from point_to_check to current sensor
+            distance = abs(point_to_check[0] - sensor[0]) + abs(point_to_check[1] - sensor[1])
+            if distance <= dist:
+                # covered
+                return True
+
+    return False
 
 def print_header():
     print("================")
-    print("= AoC - Day 14 =")
+    print("= AoC - Day 15 =")
     print("================")
 
 
-def to_point(coord: str):
-    values = coord.split(",")
-    return (int(values[0]), int(values[1]))
+def get_row_coverage(row: int, max_range: int, range_set: set):
+    range_list = []
+    for sensor in sensors.keys():
+        # get beacon
+        beacon = sensors[sensor]
+        # manatthan dist = |Xb - Xa| + |Yb - Ya|
+        # dist between beacon and sensor
+        dist = abs(beacon[0] - sensor[0]) + abs(beacon[1] - sensor[1])
+
+        x_range = dist - abs(sensor[1] - row)
+        if x_range > 0:
+            start_time = timeit.default_timer()
+            # both sides but with boundaries
+            new_left_x = sensor[0] - x_range
+            new_right_x = sensor[0] + x_range
+            [
+                range_list.append(i)
+                for i in range(
+                    new_left_x if new_left_x > 0 else 0,
+                    new_right_x + 1 if new_right_x < max_range else max_range + 1,
+                )
+            ]
+            print(f"Execution time : {timeit.default_timer() - start_time}")
+
+    return range_set.difference(range_list)
 
 
-def fill_rock_coords(p_from: tuple(int, int), p_to: tuple(int, int)):
-    global rocks
 
-    x_start = min(p_from[0], p_to[0])
-    x_end = max(p_from[0], p_to[0])
-
-    for x in range(x_start, x_end + 1):
-        rocks.add((x, p_from[1]))
-
-    y_start = min(p_from[1], p_to[1])
-    y_end = max(p_from[1], p_to[1])
-
-    for y in range(y_start, y_end):
-        rocks.add((p_from[0], y))
-
-
-def to_rock_coords(points: list(tuple(int, int))):
-    for i in range(0, len(points) - 1):
-        fill_rock_coords(points[i], points[i + 1])
-
-
-def fall_sand(fall: list, delta_x: int = 0):
-    return (fall[-1][0] + delta_x, fall[-1][1] + 1)
-
-
-def pour_sand(min_x, max_x, max_depth):
-    i = 1
-    while True:
-        fall = [SAND_START]
-        fall.append(fall_sand(fall))
-
-        nb_attempts = 0
-
-        while len(fall) <= max_depth + 1:
-            sand = fall.pop()
-            if sand not in sands and sand not in rocks:
-                fall.append(sand)
-                fall.append(fall_sand(fall))
-                nb_attempts = 0
-            else:
-                # try left
-                if nb_attempts == 0:
-                    fall.append(fall_sand(fall, -1))
-                    nb_attempts += 1
-                # try right
-                elif nb_attempts == 1:
-                    fall.append(fall_sand(fall, 1))
-                    nb_attempts += 1
-                else:
-                    # rest
-                    break
-
-        sands.add(fall[-1])
-        # draw(min_x=min_x, max_x=max_x, max_depth=max_depth)
-        if len(fall) == 1 or len(fall) > max_depth:
-            break
-        i += 1
-
-    print(i)
-
-
-def draw(min_x, max_x, max_depth):
-    for y in range(max_depth + 1):
-        for x in range(min_x, max_x + 1):
-            symb = (
-                "#"
-                if (x, y) in rocks
-                else "o"
-                if (x, y) in sands
-                else "+"
-                if (x, y) == SAND_START
-                else "."
-            )
-            print(symb, end="")
-        print("\r")
 
 
 def round_1(filename: str):
-    min_x = 1000
-    max_x = 0
-    max_depth = 0
     with open(filename) as f:
-        [
-            to_rock_coords(path)
-            for path in [
-                list(map(to_point, line.strip().split(" -> ")))
-                for line in f.readlines()
-            ]
-        ]
-        min_x = min(min(rocks, key=itemgetter(0))[0], min_x)
-        max_x = max(max(rocks, key=itemgetter(0))[0], max_x)
-        max_depth = max(max(rocks, key=itemgetter(1))[1], max_depth)
-
-        pour_sand(min_x=min_x, max_x=max_x, max_depth=max_depth)
+        [load_sensor(line.strip()) for line in f.readlines()]
+        print("row coverage: " + str(get_row_coverage(2000000)))
 
 
 def round_2(filename: str):
-    min_x = 1000
-    max_x = 0
-    max_depth = 0
+    max_range = 4000000
     with open(filename) as f:
-        [
-            to_rock_coords(path)
-            for path in [
-                list(map(to_point, line.strip().split(" -> ")))
-                for line in f.readlines()
-            ]
-        ]
-        max_depth = max(max(rocks, key=itemgetter(1))[1], max_depth)
-
-        # add last floor
-        to_rock_coords([(MIN_X, max_depth + 2), (MAX_X, max_depth + 2)])
-
-        min_x = min(min(rocks, key=itemgetter(0))[0], min_x)
-        max_x = max(max(rocks, key=itemgetter(0))[0], max_x)
-
-        pour_sand(min_x=min_x, max_x=max_x, max_depth=max_depth + 2)
-
+        [load_sensor(line.strip()) for line in f.readlines()]
+        beacon = find_excluded_beacon(max_range)
+        print("distress_beacon: ", beacon[0]*max_range + beacon[1])
 
 def main():
     print_header()
